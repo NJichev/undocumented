7b49a76e6cc94b5d5f1ba7bcb945bdae6032c304
Nikola Jichev <njichev@gmail.com>
2018-04-13 17:00:57 +0300

    Add hex meta data.

--- mix.exs
+++ mix.exs
@@ -8,6 +8,11 @@ defmodule Undocumented.MixProject do
       elixir: "~> 1.6",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
+      name: "Undocumented",
+      description: description(),
+      package: package(),
+      source_url: "https://www.github.com/njichev/undocumented",
+      homepage_url: "https://www.github.com/njichev/undocumented",
       deps: deps()
     ]
   end
@@ -26,4 +31,16 @@ defmodule Undocumented.MixProject do
 
   defp elixirc_paths(:test), do: ["lib", "test/fixtures"]
   defp elixirc_paths(_), do: ["lib"]
+
+  defp description() do
+    "A library for tracking missing documentation."
+  end
+
+  defp package() do
+    [
+      maintainers: ["njichev@gmail.com"],
+      licenses: ["MIT"],
+      links: %{github: "https://www.github.com/njichev/undocumented"}
+    ]
+  end
 end
