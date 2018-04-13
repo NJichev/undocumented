fac74df69944550ee1d68affa24cbd4f0e31575d
Nikola Jichev <njichev@gmail.com>
2018-04-13 17:01:48 +0300

    Add ExDoc

--- mix.exs
+++ mix.exs
@@ -17,16 +17,14 @@ defmodule Undocumented.MixProject do
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
-    []
+    [{:ex_doc, "~> 0.16", only: :dev, runtime: false}]
   end
 
   defp elixirc_paths(:test), do: ["lib", "test/fixtures"]
